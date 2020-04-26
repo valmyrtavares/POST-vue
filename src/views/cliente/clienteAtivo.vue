452eaac452545f60f7d7f05c6a40de5d8670a1a2
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-04-26 19:27:38 -0300

    Cheghei ao extremo do que da pra ir sem um api de verdade

--- src/views/cliente/clienteAtivo.vue
+++ src/views/cliente/clienteAtivo.vue
@@ -1,11 +1,32 @@
 <template>
 <section>
    <div>
-    <h1>Cliente Ativo</h1>
+    <h1>Cliente Ativos</h1>
       <p>nome do Cliente  {{ativo.name}}</p>
       <p>Numero da mesa  {{ativo.tableNumber}}</p>
       <p> numero de clientes  {{ativo.numCostumer}}</p>
-      <router-link to="/clientes"><p>Pedidos  {{ativo.request}}</p></router-link>
+      <!-- <router-link to="/clientes"><p>Pedidos  {{ativo.request}}</p></router-link> -->
+      <form >
+        <input type="number" v-model="prato">
+          <select id="select" v-model="tipo" >
+                <option name ="peixes" value="peixes">Peixe</option>
+                 <option name ="bebida"  value="bebidas">Bebida</option>
+                  <option name ="carne"  value="carnes">Carne</option>
+                   <option name ="massa"  value="massas">Massa</option>
+            </select>
+        <input type="submit" value="busca" @click.prevent="buscaProduto">
+      </form>
+       
+
+      <ul v-for="(prato, index) in Pedidos" :key="index">
+        <li>Prato  {{prato.nome}}</li>
+          <li>Valor  {{prato.preco}}</li>
+      </ul>
+
+
+
+     
+   
    </div>
 
          
@@ -14,13 +35,52 @@
 </template>
 
 <script>
+import {api} from "@/mixins/fetchData.js"
 export default {
     name:"clinteAtivo",
-     props:["ativo"]
+     props:["ativo"],
+     data(){
+       return{
+         tipo:"",
+         prato:"",
+         buscaPrato:null,
+         Pedidos:[],
+        //  pedido:{
+        //    prato:"",
+        //    tipo:""
+
+        //  }
+       }
+     },
+     computed:{
+       url(){
+         return `/${this.tipo}/${this.prato}`
+       }
+     },
+     methods:{
+       async buscaProduto(){
+         
+        await api.get(this.url).then(r => this.buscaPrato = r.data);
+        await this.organizaProdutos(this.buscaPrato);
+        await this. atualizandoCliente();
+       },
+       organizaProdutos(prod){
+         this.Pedidos. unshift(prod)
+       },
+         atualizandoCliente(){
+           api.put(`/costumer/${this.ativo.id}/`,this.Pedidos)
+         }
+     }
+
 
 }
 </script>
 
-<style>
+<style scoped>
+input{
+  max-width:100px;
+  height: 40px;
+}
+
 
 </style>
